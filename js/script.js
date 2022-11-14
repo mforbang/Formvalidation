7f7c78f6b568e58f572bb720e280ee969c987d1c
myasuukyiforbangdmd <57200984+mforbang@users.noreply.github.com>
2022-11-14 16:05:08 +0000

    added validation
    ...

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector('#signUpForm');
+var usernameField = document.querySelector('usernameField');
+var userNameErrorMessage = document.querySelector('#usernameErrorMessage');
+console.log(signUpForm);
+
+signUpForm.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    if(!signUpForm.username.value){
+        usernameField.classList.add('errorField');
+        usernameErrorMessage.classList.remove('hidden');
+    }
+
+    console.log(signUpForm.username.value);
+
+});
